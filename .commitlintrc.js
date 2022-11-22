cc92e9d52f35c49c6e0628f011b25d2de233dea7
foliveirafilho <franciscofilho1109@gmail.com>
2022-11-21 23:03:12 -0400

    feat: add .rc files to deps

---
+++ .commitlintrc.js
@@ -0,0 +1,31 @@
+// The commit message must have the following format:
+/*
+<type>([optional scope]): <description>
+
+[optional body]
+
+[optional footer(s)]
+*/
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    // Warns if the body message is longer than 100 characters.
+    'body-max-line-length': [1, 'always', 100],
+    // Warns if no scope were given to the commit message header.
+    'scope-empty': [1, 'never'],
+    // Errors if not of the pre-defined scopes were given
+    // Multiple scopes can be passed in a CSV way.
+    'scope-enum': [
+      1,
+      'always',
+      [
+        'deps',
+        'deps-dev',
+        // First-level directories from root directory
+        'vscode',
+        'readme',
+        'husky',
+      ],
+    ],
+  },
+};

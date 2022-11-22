ebbfd63fdbd9e99a89da51b971dbb67ece7ae1ee
Francisco Oliveira <36215435+foliveirafilho@users.noreply.github.com>
2022-11-22 00:03:19 -0400

    config: initial setup (#1)

---
+++ .eslintrc.js
@@ -0,0 +1,52 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+    sourceType: 'module',
+  },
+  plugins: ['import'],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    'no-console': 'off',
+    'no-bitwise': 'off',
+    'no-plusplus': ['error', { allowForLoopAfterthoughts: true }],
+
+    'max-len': 'off',
+
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        packageDir: '.',
+        // Files with scripts/modules that will be use only in development
+        devDependencies: ['test/*', 'jest*', 'configs/**/*', 'tools/**/*'],
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', ['index', 'internal', 'object'], ['sibling', 'parent']],
+        pathGroups: [
+          {
+            pattern: '@/**',
+            group: 'internal',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['builtin'],
+        warnOnUnassignedImports: false,
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+};
---
+++ .github/pull_request_template.md
@@ -0,0 +1,21 @@
+<!--
+
+## Altera
+
+## Adiciona
+
+## Breaking changes
+
+-->
+
+---
+
+## migration guide
+
+```bash
+git fetch
+git checkout master
+yarn --frozen-lockfile
+yarn test --runInBand
+yarn start:dev
+```

66a282a6281d70f06d11e4c9b9117c8f726c5999
Igor Vinicius <ivfernandes05@gmail.com>
2022-11-27 13:10:14 -0400

    feat(main.ts): enable cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import { AppModule } from '@/app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('CEDOMCA API')
     .setDescription('The CEDOMCA API description')

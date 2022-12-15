56fd2b3c47592e11cd29e85d86b53b34bbf3ed7e
Igor Vinicius <ivfernandes05@gmail.com>
2022-12-15 11:46:52 -0400

    feat(user): new endpoint for CRUD operations (#27)

--- src/resources/user/dto/index.ts
+++ src/resources/user/dto/index.ts
@@ -1,5 +1,6 @@
 export * from './requests/create-user.dto';
 export * from './requests/user-login.dto';
+export * from './requests/update-user.dto';
 
 export * from './responses/user-session.ro-dto';
 export * from './responses/user.ro-dto';

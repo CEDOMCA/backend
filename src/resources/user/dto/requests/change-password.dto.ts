04753225d9877f9e7460333101a3f751439aa2ea
Igor Vinicius <ivfernandes05@gmail.com>
2022-12-27 16:23:10 -0400

    feat(user): adding validation for query change password dto (#31)

--- src/resources/user/dto/requests/change-password.dto.ts
+++ src/resources/user/dto/requests/change-password.dto.ts
@@ -1,7 +1,14 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsJWT, Length } from 'class-validator';
+
 export class QueryChangePasswordDto {
+  @ApiProperty()
+  @IsJWT({ message: 'Chave de recuperação de senha não é um token válido.' })
   authKey: string;
 }
 
 export class ChangePasswordDto {
+  @ApiProperty()
+  @Length(8, 18, { message: 'Senha deve conter entre 8 e 18 caracteres.' })
   password: string;
 }

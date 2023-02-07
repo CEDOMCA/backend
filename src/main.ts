8d6c65dbc3f00fa2f263128f4bb03dcba3aa5f0c
Igor Vinicius <ivfernandes05@gmail.com>
2023-02-07 09:39:16 -0400

    fix(main): enable trust proxy

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,14 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 import { AppModule } from '@/app/app.module';
 import { setupApiSpec } from '@/docs/setup-api-spec';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.set('trust proxy', 1);
 
   /**
    * NOTE: in production, we should not expose the specification.
